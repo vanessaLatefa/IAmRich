f756e09119eb4a0482ea1f024ee2e64457763991
vanessaLatefa <vanessalatefa@gmail.com>
2019-03-09 16:56:52 +0100

    first commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Vanessa Latefa Pampilo on 2/25/19.
+//  Copyright © 2019 Vanessa Latefa Pampilo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
